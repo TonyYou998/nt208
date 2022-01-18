6baeff216079410266a3023547d4987f478be7b6
TonyYou998 <66458772+TonyYou998@users.noreply.github.com>
2022-01-18 09:02:15 +0700

    fix tag

--- models/tag.js
+++ models/tag.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
   Tag.init({
     name: {
       type: DataTypes.STRING,
-      allowNull:falsem
+      allowNull:false
     },
     description: DataTypes.STRING
   }, {
