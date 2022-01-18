2aed75272b935b88a4833b0be707de1404921aa6
TonyYou998 <66458772+TonyYou998@users.noreply.github.com>
2022-01-18 09:07:39 +0700

    add cors

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const publicPathDirectory=path.join(__dirname,"./public");
 app.use(express.json());
 
 app.use("/public",express.static(publicPathDirectory));
-
+app.use((req,res,next)=>{
+        res.setHeader('Access-Control-Allow-Origin','*');
+        res.setHeader('Access-Control-Allow-Methods','POST');
+        res.setHeader('Access-Control-Allow-Headers','*');
+        next();
+    })
 app.use("/api/v1", rootRouter);
 
 app.listen(3000,async ()=>{
