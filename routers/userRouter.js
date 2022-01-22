cb29f491c15d5683874ededa998d72efbab09a32
DangNguyen146 <19521317@gm.uit.edu.vn>
2022-01-22 20:25:16 +0700

    fix server.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const express=require('express');
 const { register, login, activateAccount } = require('../controller/user.controller');
 const { checkEmptyRegister, checkEmptyLogin } = require('../middlewares/validations/checkEmpty');
@@ -13,27 +12,3 @@ userRouter.get("/confirmation/:token",activateAccount);
 module.exports={
     userRouter,
 }
-=======
-const express = require("express");
-const {
-  register,
-  login,
-  activateAccount,
-} = require("../controller/user.controller");
-const {
-  checkEmptyRegister,
-  checkEmptyLogin,
-} = require("../middlewares/validations/checkEmpty");
-const { checkExistEmail } = require("../middlewares/validations/checkExist");
-const userRouter = express.Router();
-
-
-
-userRouter.post("/register", checkEmptyRegister, checkExistEmail, register);
-userRouter.post("/login", checkEmptyLogin, login);
-userRouter.get("/confirmation/:token", activateAccount);
-
-module.exports = {
-  userRouter,
-};
->>>>>>> Stashed changes
