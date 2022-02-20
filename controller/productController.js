e6bad03efe242e35c82130e4a01e31917b33182b
TonyYou998 <66458772+TonyYou998@users.noreply.github.com>
2022-02-20 16:44:51 +0700

    remove unnessesary ,

--- controller/productController.js
+++ controller/productController.js
@@ -64,7 +64,7 @@ const addProduct=async (req,res)=>{
 const getAllProducts=async (req,res)=>{
 
     const products=await Product.findAll({
-        attributes:['id','name','image',,'imageSau','idTypes','categoryId'],
+        attributes:['id','name','image','imageSau','idTypes','categoryId'],
     });
     if(products)
         res.status(200).send(products);
