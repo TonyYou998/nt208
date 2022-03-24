6cf93d322c88cb900dae336de5cc8c32dc6c42c7
TonyYou998 <66458772+TonyYou998@users.noreply.github.com>
2022-03-24 19:45:58 +0700

    push

--- controller/productController.js
+++ controller/productController.js
@@ -49,7 +49,7 @@ const addProduct = async (req, res) => {
 };
 const getAllProducts = async (req, res) => {
   const products = await Product.findAll({
-    attributes:['id','name','price','image','imageSau','description'],
+    attributes:['id','name','price','image','imageSau','description','rating'],
     include: [
       {
         model: Tag,
