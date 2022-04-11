b842ad5325a57f9bc9d9090a6214c007edd0b6a2
TonyYou998 <66458772+TonyYou998@users.noreply.github.com>
2022-04-11 22:25:59 +0700

    fix duplicate

--- controller/cartController.js
+++ controller/cartController.js
@@ -3,17 +3,18 @@ const addToCart=async (req,res)=>{
     const{userId,idProduct}=req.body;
 
     try {
-    //    checkProduct(idProduct);
+        const amount= checkProduct(idProduct);
+        
         const cart=await Cart.findOne({
             where:{
                 idUser:userId,
             }
         });
-        console.log(cart.id);
+        
         const newProduct=await CartProduct.create({
             idCart:cart.id,
             idProduct,
-            amount:1,
+            amount,
     
         });
         res.status(200).send({message:"add to cart success"});
@@ -26,16 +27,20 @@ const addToCart=async (req,res)=>{
 
 
 }
-// const checkProduct=async (id)=>{
-//     const product=await CartProduct.findOne({
-//         where:{
-//             idProduct:id,
-//         }
-//     });
-//     console.log(product.idProduct);
+const checkProduct=async (id)=>{
+    const product=await CartProduct.findOne({
+        where:{
+            idProduct:id,
+        }
+    });
+   if(product){
+      return product.amount++;
+       
+   }
+   return 1;
 
 
-// }
+}
 module.exports={
     addToCart,
 }
